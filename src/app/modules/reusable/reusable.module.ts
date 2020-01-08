9548fbd2810a36b0826c6f4f4324ce5fc9f104e0
Daniela Salermi <51088476+DaniSalermi@users.noreply.github.com>
2020-01-08 05:54:58 -0300

    Feature/daniela salermi/reusable module (#4)
    * add feature header
    
    * feature add footer

--- src/app/modules/reusable/reusable.module.ts
+++ src/app/modules/reusable/reusable.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HeaderAppComponent } from "./header-app/header-app.component";
 import { FooterAppComponent } from "./footer-app/footer-app.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [HeaderAppComponent, FooterAppComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderAppComponent, FooterAppComponent]
 })
 export class ReusableModule {}
