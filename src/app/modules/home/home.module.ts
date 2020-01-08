4c38ec0e55a629fe40364a11c0d62b906db8ae57
Daniela Salermi <51088476+DaniSalermi@users.noreply.github.com>
2020-01-08 06:26:04 -0300

    Feature/daniela salermi/home (#5)
    * feature add home
    
    * fix styles

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HomeRoutingModule } from './home-routing.module';
-import { IndexHomeComponent } from './index-home/index-home.component';
-
+import { HomeRoutingModule } from "./home-routing.module";
+import { IndexHomeComponent } from "./index-home/index-home.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [IndexHomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, HomeRoutingModule, RouterModule]
 })
-export class HomeModule { }
+export class HomeModule {}
