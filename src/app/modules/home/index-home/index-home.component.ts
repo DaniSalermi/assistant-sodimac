63b9d5fd68566fac1b752d3a5c942cae14cac13b
Daniela Salermi <51088476+DaniSalermi@users.noreply.github.com>
2020-01-09 15:35:18 -0300

    Revert "Feature/daniela salermi/db (#9)" (#10)
    This reverts commit 1f25de28137a12eb953234d23f4464929bc118e1.

--- src/app/modules/home/index-home/index-home.component.ts
+++ src/app/modules/home/index-home/index-home.component.ts
@@ -1,27 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { AngularFireMessaging } from "@angular/fire/messaging";
-import { typeWithParameters } from "@angular/compiler/src/render3/util";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-index-home",
-  templateUrl: "./index-home.component.html",
-  styleUrls: ["./index-home.component.scss"]
+  selector: 'app-index-home',
+  templateUrl: './index-home.component.html',
+  styleUrls: ['./index-home.component.scss']
 })
 export class IndexHomeComponent implements OnInit {
-  token: string;
-  constructor(private afMessaging: AngularFireMessaging) {}
 
-  ngOnInit() {}
+  constructor() { }
 
-  requestPermission() {
-    this.afMessaging.requestToken.subscribe(
-      token => {
-        this.token = token;
-        console.log("Permission granted! Save to the server!", token);
-      },
-      error => {
-        console.error(error);
-      }
-    );
+  ngOnInit() {
   }
+
 }
